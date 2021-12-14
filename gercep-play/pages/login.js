01ca694a619508b4e16ae81bcb2d8383ca7d8eb7
Ploeroe <raphael.hakim@student.umn.ac.id>
2021-12-15 04:40:05 +0700

    Last(?)

--- gercep-play/pages/login.js
+++ gercep-play/pages/login.js
@@ -5,6 +5,12 @@ function Login({providers}) {
         <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
 
+            <div>
+                <h1>Test 1</h1>
+                <h1>Test 2</h1>
+                <h1>Test 3</h1>
+            </div>
+
             {Object.values(providers).map((provider) => (
                 <div key={provider.name}>
                     <button className="bg-[#18D860] text-white p-5 
